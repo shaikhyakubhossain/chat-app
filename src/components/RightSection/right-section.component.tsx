df981d6a15c9af9826280b74021adb8a29d69ed7
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-04 14:42:37 +0530

    Add RightSection component

---
+++ src/components/RightSection/right-section.component.tsx
@@ -0,0 +1,14 @@
+import styles from './right-section.module.scss';
+import Nav from '../Nav/nav.component';
+import MainChat from '../MainChat/main-chat.component';
+
+
+
+export default function RightSection(): JSX.Element {
+    return (
+        <div className={`${styles.mainContainer}`}>
+            <Nav />
+            <MainChat />
+        </div>
+    )
+}
