87b45019dc38f23dea39245039c09316cedef512
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-04 07:53:10 +0530

    Add LeftMenu component

---
+++ src/components/LeftMenu/left-menu.component.tsx
@@ -0,0 +1,12 @@
+import styles from './left-menu.module.scss'
+
+
+
+export default function LeftMenu(): JSX.Element {
+
+    return (
+        <div className={`${styles.mainContainer} w-1/4`}>
+            
+        </div>
+    )
+}
