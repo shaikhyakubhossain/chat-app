66614662bc48495c03b2d46aeb73db90fbd9fd10
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-04 07:26:36 +0530

    Update css

--- src/components/Nav/nav.component.tsx
+++ src/components/Nav/nav.component.tsx
@@ -5,12 +5,12 @@ import styles from './nav.module.scss'
 export default function Nav(): JSX.Element {
 
     return (
-        <div className={`${styles.navContainer} flex fixed items-center top-0 left-0 right-0  justify-between bg-yellow-50 h-16 text-black`}>
+        <div className={`${styles.navContainer} flex fixed items-center top-0 left-0 right-0  justify-between bg-white h-16 text-black`}>
         <div className='flex'>
-            <div className={'w-6 h-6 rounded-full bg-black'}></div>
+            <div className={'w-6 h-6 rounded-full bg-slate-500'}></div>
             <div>
-                <div>person-1</div>
-                <div></div>
+                <div className={`${styles.title} font-bold`}>person-1</div>
+                <div className={`${styles.status} text-slate-500`}>online</div>
             </div>
         </div>
         <div>
