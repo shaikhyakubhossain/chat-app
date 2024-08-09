44ae6403058decdc4e4aafad538eeb717764b30e
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-09 11:25:35 +0530

    Update ConversationCard component

--- src/components/ConversationCard/conversation-card.component.tsx
+++ src/components/ConversationCard/conversation-card.component.tsx
@@ -4,20 +4,24 @@ import styles from './conversation-card.module.scss'
 
 
 type PropsType = {
-
+    title: string,
+    lastMessage?: string,
+    lastMessageTime?: string,
+    profileImg?: string,
+    numberOfUnreadMessages?: number
 }
 
 export default function ConversationCard(props: PropsType) {
     return (
-        <div className={`${styles.conversationCard}`}>
-            <div className={`${styles.profileImg}`}><img src="" alt="" /></div>
+        <div className={`${styles.conversationCard} flex`}>
+            <div className={`${styles.profileImg}`}><img src={props.profileImg} alt={props.title} /></div>
             <div>
-                <div></div>
-                <div></div>
+                <div>{ props.title }</div>
+                <div>{ props.lastMessage }</div>
             </div>
             <div>
-                <div></div>
-                <div></div>
+                <div>{ props.lastMessageTime }</div>
+                <div>{ props.numberOfUnreadMessages }</div>
             </div>
         </div>
     )
