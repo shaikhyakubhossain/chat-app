aa3622b2746f206ddf5adb974a1cf878b608df20
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-09 09:27:41 +0530

    Add constants in utils

---
+++ src/utils/constants.ts
@@ -0,0 +1,31 @@
+
+export const connectedUsers = [
+    {
+        name: 'person-1',
+        status: 'online'
+    },
+    {
+        name: 'person-2',
+        status: 'offline'
+    },
+    {
+        name: 'person-3',
+        status: 'online'
+    },
+    {
+        name: 'person-4',
+        status: 'offline'
+    }
+]
+
+export const connectedGroups = [
+    {
+        name: 'Public group chat',
+        status: 'default'
+    },
+    {
+        name: 'group-1',
+        status: 'online'
+    }
+    
+]
