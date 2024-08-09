80b9e44ddf567956c11c3674d5a6271287f5bf5c
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-09 11:56:55 +0530

    Update LeftMenu component

--- src/components/LeftMenu/left-menu.component.tsx
+++ src/components/LeftMenu/left-menu.component.tsx
@@ -1,12 +1,19 @@
 import styles from './left-menu.module.scss';
 import ConversationCard from '../ConversationCard/conversation-card.component';
+import { conversationsList } from '@/utils/constants';
 
 
 export default function LeftMenu(): JSX.Element {
 
     return (
-        <div className={`${styles.mainContainer} w-1/4`}>
-            <ConversationCard />
+        <div className={`${styles.mainContainer} mt-16`}>
+            {
+                conversationsList.map((item, index): JSX.Element => {
+                    return (
+                        <ConversationCard key={index} title={item.name} /> 
+                    )
+                }
+            )}
         </div>
     )
 }
