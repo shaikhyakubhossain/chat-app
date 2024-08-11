ecfb223724dbda575aa4b40c87990e47055262cc
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-11 14:42:31 +0530

    Update MessageBubble css

--- src/components/MessageBubble/message-bubble.component.tsx
+++ src/components/MessageBubble/message-bubble.component.tsx
@@ -24,8 +24,8 @@ export default function MessageBubble(props: propsType): JSX.Element {
     return (
         <div className={`${styles.mainContainer} flex ${toggleSide} text-center`}>
             <div className={`text-slate-800 ${toggleColor} rounded my-2 px-4 py-3`}>
+            <div className={`text-sm text-left`}>{props.sentBy}</div>
             <div className={`text-2xl`}>{props.message}</div>
-            <div className={`text-sm text-right`}>{props.sentBy}</div>
             </div>
             
         </div>
