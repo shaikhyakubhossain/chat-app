acbb8f3906cdf427cdd427ab083bcd4da17c640b
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-11 16:12:53 +0530

    Update css, minor changes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,28 +1,14 @@
 
 export const conversationsList = [
     {
-        name: 'Public Group',
+        name: 'Public Group Chat',
         status: 'online',
         type: 'group'
     },
     {
-        name: 'person-1',
-        status: 'offline',
-        type: 'individual'
+        name: 'test',
+        status: 'online',
+        type: 'group'
     },
-    {
-        name: 'person-2',
-        status: 'offline',
-        type: 'individual'
-    },
-    {
-        name: 'person-3',
-        status: 'offline',
-        type: 'individual'
-    },
-    {
-        name: 'person-4',
-        status: 'offline',
-        type: 'individual'
-    }
+    
 ]
