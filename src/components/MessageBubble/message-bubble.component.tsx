6b170c32b4be3c46ec27741016d426fdc83b2f42
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-11 14:27:45 +0530

    improve MessageBubble css, and Nav css

--- src/components/MessageBubble/message-bubble.component.tsx
+++ src/components/MessageBubble/message-bubble.component.tsx
@@ -8,11 +8,22 @@ type propsType = {
 
 export default function MessageBubble(props: propsType): JSX.Element {
 
-    const toggleColor = props.sentBy === 'you' ? 'bg-green-500' : 'bg-white'
+    let toggleColor;
+    let toggleSide;
+
+    if(props.sentBy === 'you'){
+        toggleColor = 'bg-green-500';
+        toggleSide = 'justify-end';
+    }
+    else{
+        toggleColor = 'bg-white';
+        toggleSide = 'justify-start';
+    }   
+
 
     return (
-        <div className={`${styles.mainContainer} ${toggleColor}`}>
-            <div className="text-right text-slate-800">{props.sentBy + ": " + props.message}</div>
+        <div className={`${styles.mainContainer} flex ${toggleSide} text-center`}>
+            <span className={`text-slate-800 ${toggleColor} rounded my-2 px-4 py-3`}>{props.sentBy + ": " + props.message}</span>
         </div>
     )
 }
