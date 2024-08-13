8f5cc32c9e27514f562fe66bfaa75abf99f6554c
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-13 20:44:59 +0530

    Add RTKStoreProvider component

---
+++ src/components/RTKStoreProvider/rtk-store-provider.component.tsx
@@ -0,0 +1,14 @@
+import styles from './rtk-store-provider.module.scss';
+
+
+
+export default function RTKStoreProvider(): JSX.Element {
+
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.mainContainer}>
+            
+            </div>
+        </div>
+    )
+}
