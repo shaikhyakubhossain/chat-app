5126b2eb6a64e956a5be946ca63f5a3d2c47d553
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-15 13:49:11 +0530

    Add MessageInput component

---
+++ src/components/MessageInput/message-input.component.tsx
@@ -0,0 +1,17 @@
+import styles from './message-input.module.scss';
+import Btn from '../Btn/btn.component';
+
+
+
+type propsType = {
+    sendMessageFunction: () => void,
+}
+
+export default function MessageInput(props: propsType): JSX.Element {
+    return (
+        <div className={`${styles.mainContainer} flex`}>
+            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="message" />
+            <Btn onClick={props.sendMessageFunction} customTW='bg-pink-700 hover:bg-pink-800'>Send</Btn>
+        </div>
+    )
+}
