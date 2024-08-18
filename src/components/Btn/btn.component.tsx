25c2d59530303feb14abba8167d7cc1c94352c31
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-08-18 10:53:29 +0530

    Minor css improvement

--- src/components/Btn/btn.component.tsx
+++ src/components/Btn/btn.component.tsx
@@ -3,14 +3,15 @@ import styles from './btn.module.scss';
 type propsType = {
     children: string,
     onClick: (event: React.MouseEvent<HTMLButtonElement>) => void,
-    customTW?: string
+    customTW?: string,
+    customClass?: string
 }
 
 
 export default function Btn(props: propsType): JSX.Element {
     return (
         <div className={`${styles.mainContainer} my-auto`}>
-            <button onClick={props.onClick} className={`focus:outline-none text-white hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 my-auto ${props.customTW}`}>{props.children}</button>
+            <button onClick={props.onClick} className={`${styles.btn} ${props.customClass} focus:outline-none text-white hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 my-auto ${props.customTW}`}>{props.children}</button>
         </div>
     )
 }
--- src/components/MessageInput/message-input.component.tsx
+++ src/components/MessageInput/message-input.component.tsx
@@ -10,8 +10,8 @@ type propsType = {
 export default function MessageInput(props: propsType): JSX.Element {
     return (
         <div className={`${styles.mainContainer} flex`}>
-            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="message" />
-            <Btn onClick={props.sendMessageFunction} customTW='bg-pink-700 hover:bg-pink-800'>Send</Btn>
+            <input type="text" className={`${styles.textField} bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="message" />
+            <Btn onClick={props.sendMessageFunction} customClass={styles.sendBtn} customTW='bg-pink-700 hover:bg-pink-800'>Send</Btn>
         </div>
     )
 }
