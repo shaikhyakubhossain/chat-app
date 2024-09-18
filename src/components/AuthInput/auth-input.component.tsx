d4f4b17ff3b8d4dc7db4bd79cd2a2ba1500ea2dd
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-09-18 16:26:52 +0530

    Update AuthInput, and UserAuth components

--- src/components/AuthInput/auth-input.component.tsx
+++ src/components/AuthInput/auth-input.component.tsx
@@ -1,11 +1,33 @@
+"use client";
+import { useState } from 'react';
 import styles from './auth-input.module.scss';
+import Btn from '../Btn/btn.component';
 
+type dataToSendType = {
+    username: string,
+    password: string,
+    confirmPassword?: string,
+}
 
 type propsType = {
-    type: string | null
+    type: string | null,
+    submit: ({username, password}: dataToSendType) => void,
+    status?: string
 }
 
 const AuthInput = (props: propsType) => {
+
+    const [dataToSend, setDataToSend] = useState<dataToSendType>({username: '', password: '', confirmPassword: ''});
+
+    const handleSubmit = () => {
+        if(props.type === 'login'){
+            props.submit(dataToSend);
+        }
+        else if(props.type === 'register' && dataToSend.password === dataToSend.confirmPassword && dataToSend.password.length > 5){
+            props.submit(dataToSend);
+        }
+    }
+
     return (
         <div className={styles.authInputContainer}>
             {
@@ -13,22 +35,29 @@ const AuthInput = (props: propsType) => {
             }
             <div className={`${styles.textFieldContainer}`}>
                     <div className="mb-6">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                        <input type="email" id="email" className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="username" required />
+                        <label className="block mb-2 text-sm font-medium text-gray-900">Username</label>
+                        <input onChange={(e) => setDataToSend({...dataToSend, username: e.target.value})} type="text" id="username" className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="username" required />
                     </div> 
+                    <div className={`${props.status === '400' ? 'block' : 'hidden'} text-red-800`}>Username already taken</div>
                     <div className="mb-6">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" id="password" className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="password" required />
+                        <label className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                        <input onChange={(e) => setDataToSend({...dataToSend, password: e.target.value})} type="password" id="password" className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="password" required />
                     </div> 
+                    <div className={`${props.status === '400' ? 'block' : 'hidden'} text-red-800`}>Password must be at least 6 characters</div>
+                    <div className={`${props.status === '400' ? 'block' : 'hidden'} text-red-800`}>Passwords do not match</div>
+
                     {
                         props.type === 'register' && (
                         <div className="mb-6">
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                            <input type="password" id="confirm_password" className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="confirm password" required />
+                            <label className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                            <input onChange={(e) => setDataToSend({...dataToSend, confirmPassword: e.target.value})} type="password" id="confirm_password" className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`} placeholder="confirm password" required />
                         </div>
                         )
                     }
                 </div>
+                <div className='flex justify-center'>
+                <Btn customTW='bg-pink-500' onClick={handleSubmit}>{props.type === 'login' ? 'Login' : 'Register'}</Btn>
+            </div>
         </div>
     )
 }
--- src/components/UserAuth/user-auth.component.tsx
+++ src/components/UserAuth/user-auth.component.tsx
@@ -1,29 +1,65 @@
-import styles from './user-auth.module.scss';
+"use client";
+import styles from "./user-auth.module.scss";
+import { useState } from "react";
+import AuthInput from "../AuthInput/auth-input.component";
+import Btn from "../Btn/btn.component";
 
+type dataToSendType = {
+  username: string;
+  password: string;
+};
 
 export default function UserAuth(): JSX.Element {
-    return (
-        <div className={`${styles.mainContainer} w-3/4 mx-auto`}>
-            <div className={`${styles.textFieldContainer}`}>
-                    <div className="mb-6">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
-                        <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
-                    </div> 
-                    <div className="mb-6">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
-                    </div> 
-                    <div className="mb-6">
-                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                        <input type="password" id="confirm_password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
-                    </div> 
-                    <div className="flex items-start mb-6">
-                        <div className="flex items-center h-5">
-                        <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
-                        </div>
-                        <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
-                    </div>
-            </div>
+
+  const [authType, setAuthType] = useState<null | string>("login");
+  const [data, setData] = useState<string>("");
+
+  const handleLoginOrSignUp = (dataToSend: dataToSendType) => {
+      fetch(`http://localhost:4000/${authType}`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: dataToSend.username,
+          password: dataToSend.password,
+        }),
+      }).then((res) => {
+        console.log(res.json().then((data) => console.log(data)));
+        if(res.status === 400){
+          setData(res.status.toString());
+        }
+        else if(res.status === 200){
+          setData(res.status.toString());
+        }
+      });
+  };
+
+  return (
+    <div className={`${styles.mainContainer} w-3/4 m-auto`}>
+      <div className="font-bold text-5xl text-center mb-4">
+        Welcome to Chat App!
+      </div>
+      <div className="flex justify-center">
+        <div>
+          <Btn
+            customTW="bg-green-500 mr-2"
+            onClick={() => setAuthType("login")}
+          >
+            login
+          </Btn>
         </div>
-    )
+        <div>
+          <Btn
+            customTW="bg-green-500 ml-2"
+            onClick={() => setAuthType("register")}
+          >
+            Create Account
+          </Btn>
+        </div>
+      </div>
+      {<AuthInput type={authType} submit={handleLoginOrSignUp} status={data} />}
+    </div>
+  );
 }
