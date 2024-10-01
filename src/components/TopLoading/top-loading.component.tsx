de5def7dcdd02b0008a831fc2adf3abeaf62a8d5
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-02 02:14:39 +0530

    Add TopLoading component

---
+++ src/components/TopLoading/top-loading.component.tsx
@@ -0,0 +1,14 @@
+import styles from "./top-loading.module.scss";
+
+import { RootState } from "@/lib/store";
+import { useSelector } from "react-redux";
+
+export default function TopLoading(){
+
+    const showLoading = useSelector((state: RootState) => state.mainLoading.showLoading);
+
+    return (
+        <div className={`${styles.mainContainer} ${showLoading ? styles.widthFull : styles.widthZero} fixed top-0 left-0 bg-red-400 h-1`}>
+        </div>
+    )
+}
