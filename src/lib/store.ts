85715545b3647737d3454f19f2543edc0094cd55
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-10-02 02:15:55 +0530

    Update store.ts

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import mainMobileWindowSlice from "./features/MainMobileWindow/mainMobileWindowSlice";
 import navActiveChatSlice from "./features/navActiveChat/navActiveChatSlice";
 import authDetailSlice from './features/AuthDetail/authDetailSlice';
+import mainLoadingSlice from "./features/MainLoading/mainLoadingSlice";
 
 export const store = configureStore({
     reducer: {
         mainMobileWindow: mainMobileWindowSlice,
         navActiveChat: navActiveChatSlice,
-        authDetail: authDetailSlice
+        authDetail: authDetailSlice,
+        mainLoading: mainLoadingSlice
     }
 });
 
