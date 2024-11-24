c971227d977f8dd131d5050bfaa81bce2f9123f7
Yakub Hossain <shaikhyakubhossain@gmail.com>
2024-11-24 23:53:40 +0530

    Update character limit from 250 to 150 in MainChat component

--- src/components/MainChat/main-chat.component.tsx
+++ src/components/MainChat/main-chat.component.tsx
@@ -49,7 +49,7 @@ export default function MainChat(): JSX.Element {
                 friendName: title
             })
         }).then(res => res.json()).then(data => {
-            console.log(data.data);
+            console.log("fetched new");
             modifyMessageList(data.data);
         })
     }
@@ -61,7 +61,7 @@ export default function MainChat(): JSX.Element {
     }
 
     const sendMessage = () => {
-        if((containerOfInputRef.current?.children[1].children[0] as HTMLInputElement).value.length <= 250){
+        if((containerOfInputRef.current?.children[1].children[0] as HTMLInputElement).value.length <= 150){
             const inputValue = (containerOfInputRef.current?.children[1].children[0] as HTMLInputElement);
             // console.log(inputValue.value)
             if(ws && containerOfInputRef.current?.children[1].children[0] && inputValue.value !== ""){
@@ -96,7 +96,7 @@ export default function MainChat(): JSX.Element {
             {ws ? <ChatBox serverMessage={messagesList} /> : <div className="mb-auto bg-lime-600 text-white text-center rounded p-2">Connecting to the server...</div>}
             <div className='flex justify-center '>
                 <div ref={containerOfInputRef} className="w-3/4 mx-auto">
-                    <div className={`${styles.messageTooLongError} mb-auto bg-red-500 text-white rounded p-2`}>Message too long, max 250 characters</div>
+                    <div className={`${styles.messageTooLongError} mb-auto bg-red-500 text-white rounded p-2`}>Message too long, max 150 characters</div>
                     <MessageInput sendMessageFunction={sendMessage}/>
                 </div>
             </div>
